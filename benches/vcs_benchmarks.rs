702b7721d7a4ce9208ae5c8b38a78e125865156a
nicola <nicola@users.noreply.github.com>
2019-05-15 17:44:19 +0200

    running benchmarks

--- benches/vcs_benchmarks.rs
+++ benches/vcs_benchmarks.rs
@@ -10,7 +10,7 @@ extern crate rand_chacha;
 extern crate criterion;
 extern crate blake2;
 
-use criterion::Criterion;
+use criterion::{Criterion, Benchmark};
 
 //These benches are taken from various places.
 
@@ -19,30 +19,68 @@ mod vc_benches {
     use accumulators::group::RSAGroup;
     use accumulators::traits::{BatchedAccumulator, StaticAccumulator, StaticVectorCommitment};
     use accumulators::Accumulator;
-    use accumulators::vc::binary;
+    use accumulators::vc::{binary, yinyan};
     use num_bigint::RandPrime;
     use rand::{Rng, SeedableRng};
     use rand_chacha::ChaChaRng;
 
-    const N: usize = 2048;
-    const L: usize = 256;
+    const N: usize = 1048;
+    const L: usize = 128;
 
-    fn bench_bbf(c: &mut Criterion) {
-        let m = 64;
+    // fn make_vc() -> (binary::BinaryVectorCommitment, yinyan::YinYanVectorCommitment) {
+    //     let m = 10;
+    //     let mut rng = ChaChaRng::from_seed([0u8; 32]);
+
+    //     let config_bbf = binary::Config { lambda: L, n: N };
+    //     let mut vc_bbf = binary::BinaryVectorCommitment::<Accumulator>::setup::<RSAGroup, _>(&mut rng, &config_bbf);
+
+    //     let config_yy = yinyan::Config { lambda: L, k: 1, n: N, size: m };
+    //     let mut vc_yy = yinyan::YinYanVectorCommitment::<Accumulator>::setup::<RSAGroup, _>(&mut rng, &config_yy);
+
+    //    vc_yy, vc_bbf
+    // }
+
+    fn bench_bbf_commit(c: &mut Criterion) {
+        let m = 10;
         let mut rng = ChaChaRng::from_seed([0u8; 32]);
 
-        let config = binary::Config { lambda: L, n: N };
-        let mut vc = binary::BinaryVectorCommitment::<Accumulator>::setup::<RSAGroup, _>(&mut rng, &config);
+        // setting up BBF
+        let config_bbf = binary::Config { lambda: L, n: N };
+        let mut vc_bbf = binary::BinaryVectorCommitment::<Accumulator>::setup::<RSAGroup, _>(&mut rng, &config_bbf);
+        let mut val_bbf: Vec<bool> = (0..m).map(|_| rng.gen()).collect();
+        val_bbf[3] = true;
 
-        let val: Vec<bool> = (0..64).map(|_| rng.gen()).collect();
-        c.bench_function("bench_bbf", move |b| b.iter(|| vc.commit(&val)));
+        // setting up YY
+        let config_yy = yinyan::Config { lambda: L, k: 1, n: N, size: m };
+        let mut vc_yy = yinyan::YinYanVectorCommitment::<Accumulator>::setup::<RSAGroup, _>(&mut rng, &config_yy);
+        let val_yy : Vec<Vec<bool>> = val_bbf.iter().map(|v| vec![*v]).collect();
+
+        // Run Commit benchmarks
+        {
+            let (mut vc_bbf2, mut vc_yy2) = (vc_bbf.clone(), vc_yy.clone());
+            let (val_bbf2, val_yy2) = (val_bbf.clone(), val_yy.clone());
+
+            c
+                .bench_function("bench_bbf_commit", move |b| b.iter(|| vc_bbf2.commit(&val_bbf2)))
+                .bench_function("bench_yinyan_commit", move |b| b.iter(|| vc_yy2.commit(&val_yy2)));
+        }
+
+        vc_bbf.commit(&val_bbf);
+        vc_yy.commit(&val_yy);
+
+        // Run Open benchmarks
+        {
+            c
+                .bench_function("bench_bbf_open", move |b| b.iter(|| vc_bbf.open(&true, 3) ))
+                .bench_function("bench_yinyan_open", move |b| b.iter(|| vc_yy.open(&vec![true], 3) ));
+        }
     }
 
     criterion_group! {
         name = vc_benches;
-        config = Criterion::default();
+        config = Criterion::default().sample_size(3);
         targets =
-            bench_bbf,
+            bench_bbf_commit,
     }
 
 }
